df669d1780ce37f26921b4000e5f195188222381
piotr <p.kacprzak.96@gmail.com>
2019-12-22 19:34:32 +0100

    Added simole convolutional network, started using tensorboard

---
+++ convolutional_network.py
@@ -0,0 +1,41 @@
+import pickle
+import time
+
+import numpy as np
+from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.layers import Dense, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.models import Sequential
+
+# print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+NAME = f"road-signs-recognition-conv-net-64x2-{int(time.time())}"
+
+tensorboard = TensorBoard(log_dir=f'logs/{NAME}')
+
+X = np.array(pickle.load(open("pickled_datasets/X.pickle", "rb")))
+y = np.array(pickle.load(open("pickled_datasets/y.pickle", "rb")))
+categories_number = len(np.unique(y))
+print(f"Number of categories: {categories_number}")
+
+X = X / 255.0
+
+model = Sequential()
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+
+model.add(Dense(categories_number))
+model.add(Activation("sigmoid"))
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="adam",
+              metrics=["accuracy"])
+
+model.fit(X, y, batch_size=8, epochs=10, validation_split=0.1, callbacks=[tensorboard])
---
+++ prepare_dataset.py
@@ -0,0 +1,49 @@
+import os
+import pickle
+import random
+
+import cv2
+import numpy as np
+
+IMAGESDIR = "/home/piotr/Obrazy"
+PICKLEDDIR = "pickled_datasets"
+CATEGORIES = ["20", "30", "40", "50", "70", "koniec pierwszenstwa", "masz_pierwszenstwo", "przejscie dla pieszych",
+              "stop"]
+IMG_WIDTH = 50
+IMG_HEIGHT = 50
+
+
+def create_dataset():
+    training_dataset = []
+    for category_name in CATEGORIES:
+        category_path = os.path.join(IMAGESDIR, category_name)
+        category_number = CATEGORIES.index(category_name)
+        for img in os.listdir(category_path):
+            try:
+                training_dataset.append([cv2.resize(cv2.imread(os.path.join(category_path, img), cv2.IMREAD_GRAYSCALE),
+                                                    (IMG_WIDTH, IMG_HEIGHT)), category_number])
+            except Exception as e:
+                print(e)
+    random.shuffle(training_dataset)
+    return training_dataset
+
+
+def reshape_dataset(dataset):
+    X = []
+    y = []
+    for array, label in dataset:
+        X.append(array)
+        y.append(label)
+    X = np.array(X).reshape(-1, IMG_HEIGHT, IMG_WIDTH, 1)
+    print(y)
+    return X, y
+
+
+training_dataset = create_dataset()
+X, y = reshape_dataset(training_dataset)
+pickle_out = open(f"{PICKLEDDIR}/X.pickle", "wb")
+pickle.dump(X, pickle_out)
+pickle_out.close()
+pickle_out = open(f"{PICKLEDDIR}/y.pickle", "wb")
+pickle.dump(y, pickle_out)
+pickle_out.close()
